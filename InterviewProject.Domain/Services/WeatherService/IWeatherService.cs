562731304e2fe290ee4d27a88ed95fd6f9578ea3
Pat Brennan <pbrennan@seityhealth.com>
2023-01-05 18:03:30 -0500

    creates concreate weather service

---
+++ InterviewProject.Domain/Services/WeatherService/IWeatherService.cs
@@ -0,0 +1,11 @@
+﻿using InterviewProject.Domain.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace InterviewProject.Domain.Services.WeatherService
+{
+  public interface IWeatherService
+  {
+    Task<IEnumerable<WeatherForecast>> GetWeatherForecast(string location);
+  }
+}
---
+++ InterviewProject.Domain/Services/WeatherService/WeatherService.cs
@@ -0,0 +1,15 @@
+﻿using InterviewProject.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace InterviewProject.Domain.Services.WeatherService
+{
+  public class WeatherService : IWeatherService
+  {
+    public Task<IEnumerable<WeatherForecast>> GetWeatherForecast(string location)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
