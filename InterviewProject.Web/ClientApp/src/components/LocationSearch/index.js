567dae4ce58bc33c90f06b9e8f65a478320563f7
Pat Brennan <pbrennan@seityhealth.com>
2023-01-08 10:13:26 -0500

    moves location search and adds tests

---
+++ InterviewProject.Web/ClientApp/src/components/LocationSearch/index.js
@@ -0,0 +1,31 @@
+﻿import React, { useState } from 'react';
+
+const LocationSearch = (props) => {
+    const [postalCode, setPostalCode] = useState('');
+
+    const handlePostalCodeInputChance = (event) => {
+        setPostalCode(event.target.value);
+    }
+
+    return (
+        <div className='row'>
+            <form onSubmit={event => props.handleSubmit(event, postalCode)}>
+                <div className='input-group mb-3 col-xs-3'>
+                    <input
+                        onChange={handlePostalCodeInputChance}
+                        value={postalCode}
+                        placeholder='Enter Postal Code'
+                        name='postalCode'
+                        className='form-control input-lg'
+                        required
+                    />
+                    <div class='input-group-append'>
+                        {props.isLoading ? <button class='btn btn-outline-secondary' data-testId='locations-loading'>Loading...</button> : <button class='btn btn-outline-secondary' type='submit' data-testId='locations-search'>Seach</button>}
+                    </div>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default LocationSearch;
---
+++ InterviewProject.Web/ClientApp/src/components/__tests__/LocationSearch.tests.js
@@ -0,0 +1,31 @@
+﻿import { cleanup, fireEvent, render } from '@testing-library/react';
+import LocationSearch from '../LocationSearch';
+
+test('test', () => {
+    expect(true).toBe(true);
+});
+
+describe(Locations, () => {
+    it('should render locations loading text', () => {
+        render(<LocationSerach handleSubmit={() => { }} isLoading={true} />);
+        const loadingElement = screen.getByTestId('locations-loading');
+        expect(loadingElement).toBeInTheDocument();
+    });
+});
+
+//test('should render locations loading screen', () => {
+//    render(<Locations locations={[]} isLoading={true} handleLocationClicked={() => { }} />);
+//    const loadingElement = screen.getByTestId('locations-loading');
+//    expect(loadingElement).toBeInTheDocument();
+//});
+
+//test('should render locations list', () => {
+//    const locations = [
+//        { key: 1, name: "Philly" },
+//        { key: 2, name: "New York" }
+//    ]
+
+//    render(<Locations locations={locations} isLoading={false} handleLocationClicked={() => { }} />);
+//    const loadingElement = screen.getByTestId('locations-list');
+//    expect(loadingElement).toBeInTheDocument();
+//});
