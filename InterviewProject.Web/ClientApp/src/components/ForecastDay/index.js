07af8ecea0e22ac7423e3d22f29a0184a7d3f77f
Pat Brennan <pbrennan@seityhealth.com>
2023-01-07 22:53:39 -0500

    updates styles

--- InterviewProject.Web/ClientApp/src/components/ForecastDay/index.js
+++ InterviewProject.Web/ClientApp/src/components/ForecastDay/index.js
@@ -7,9 +7,13 @@ const ForecastDay = (props) => {
     console.log("Forecast day props", props);
     return (
         <div className='forecast-day-container'>
-            <div>{moment(props.forecastDay.date).format('dddd, MMMM D, YYYY')}</div>
-            <div>High Temp: {props.forecastDay.highTemperature}</div>
-            <div>Low Temp: {props.forecastDay.lowTemperature}</div>
+            <div className='forecast-day-date'>{moment(props.forecastDay.date).format('ddd, MMM D')}</div>
+            <span className='forecast-day-high'>{props.forecastDay.highTemperature}°</span>
+            <span>/</span>
+            <span className='forecast-day-low'>{props.forecastDay.lowTemmperature}°</span>
+            <div>
+
+            </div>
         </div>
     )
 }
