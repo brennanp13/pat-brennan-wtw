384ca8efbf948cf3142f987f8ccb37590352a795
Pat Brennan <pbrennan@seityhealth.com>
2023-01-09 13:38:31 -0500

    updates forecast days tests

--- InterviewProject.Web/ClientApp/src/components/__tests__/ForecastDays.tests.js
+++ InterviewProject.Web/ClientApp/src/components/__tests__/ForecastDays.tests.js
@@ -11,7 +11,7 @@ describe('Location Search', () => {
     });
 
     it('should display location and forecast', () => {
-        const locationName = 'test';
+        const locationName = 'test location name';
         const forecastDays = [
             { date: "1/1/2023", highTemperature: 90, lowTemmperature: 40, phrase: "Test phrase 1" },
             { date: "1/2/2023", highTemperature: 91, lowTemmperature: 41, phrase: "Test phrase 2" },
@@ -20,12 +20,13 @@ describe('Location Search', () => {
             { date: "1/5/2023", highTemperature: 94, lowTemmperature: 44, phrase: "Test phrase 5" }
         ]
 
-        const { getByTestId } = render(<ForecastDays locationName={locationName} forecastDays={forecastDays} isLoading={false} hasError={false} />);
+        render(<ForecastDays locationName={locationName} forecastDays={forecastDays} isLoading={false} hasError={false} />);
         expect(getByTestId('forecastdays-list')).toBeInTheDocument();
+        expect(getByText(locationName)).toBeInTheDocument();
     });
 
     it('should render error message', () => {
-        const { getByTestId } = render(<ForecastDays locationName={'test'} forecastDays={[]} isLoading={false} hasError={true} />);
+        render(<ForecastDays locationName={'test'} forecastDays={[]} isLoading={false} hasError={true} />);
         expect(getByTestId('forecastdays-error')).toBeInTheDocument();
     });
 });
